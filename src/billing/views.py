d864355654aad2e81a5e795872e914739b837249
topaz1874 <topaz1874@gmail.com>
2016-09-13 11:26:44 +0800

    unique subscription part 2

--- src/billing/views.py
+++ src/billing/views.py
@@ -30,8 +30,6 @@ def upgrade(request):
             messages.success(request, 'Hoho, now you become our site member, enjoy.')
             return HttpResponseRedirect(next_url)
 
-        merchant_customer_id = None
-
         try:
             #usermerchantid already created in userprofile post save handler
             merchant_obj = UserMerchantID.objects.get(user=request.user)
@@ -41,81 +39,88 @@ def upgrade(request):
 
         merchant_customer_id = merchant_obj.customer_id
         merchant_subscription_id = merchant_obj.subscription_id
-        merchant_plan_id = merchant_obj.plan_id
+        # merchant_plan_id = merchant_obj.plan_id
 
         # receive a payment method nonce from client
         # that means that server got customer's payment authorization
-        nonce = request.POST.get('payment_method_nonce', None)
-        if nonce is not None:
-            payment_method_result = braintree.PaymentMethod.create({
-                "customer_id": merchant_customer_id,
-                "payment_method_nonce": nonce,
-                "options": {
-                    "make_default": True }
-                })
-            if not payment_method_result.is_success:
-                messages.error(request, "An error occured: %s" % (payment_method_result.message))
+        if request.method == 'POST':
+            nonce = request.POST.get('payment_method_nonce', None)
+            if nonce is None:
+                messages.error(request, "An error occured, please try again.")
                 return redirect('upgrade')
-            the_token = payment_method_result.payment_method.token
+            else:
+                payment_method_result = braintree.PaymentMethod.create({
+                    "customer_id": merchant_customer_id,
+                    "payment_method_nonce": nonce,
+                    "options": {
+                        "make_default": True }
+                    })
+                if not payment_method_result.is_success:
+                    messages.error(request, "An error occured: %s" % (payment_method_result.message))
+                    return redirect('upgrade')
 
-            # if subscription_id is not none then update subscription
-            if merchant_subscription_id is not None and merchant_plan_id is not None:
-                #make the subscription status is active
-                current_subscription_result = braintree.Subscription.find(merchant_subscription_id)
-                if current_subscription_result.status == "Active":
-                    #not finish yet
-                    subscription_result = braintree.Subscription.update(
-                        merchant_subscription_id,{
+                the_token = payment_method_result.payment_method.token
+                did_create_sub = False
+                did_update_sub = False
+                trans_success = False
+                try:
+                    current_subscription_result = braintree.Subscription.find(merchant_subscription_id)
+                    current_subscription_status = current_subscription_result.status
+                except:
+                    current_subscription_result = None
+                    current_subscription_status = None
+                # if 1 then update subscription
+                if current_subscription_result and current_subscription_status == "Active":
+                    subscription_result = braintree.Subscription.update(merchant_subscription_id,{
                         "payment_method_token": the_token,
                         })
-                    if subscription_result.is_success:
-                        messages.success(request, "You've renewed subscription successed, thanks!")
-                        return redirect('history')
-                #not active create an new subscription
+                    did_update_sub = True
+                # create an subscription
                 else:
                     subscription_result = braintree.Subscription.create({
                         "payment_method_token": the_token,
                         "plan_id":PLAN_ID,
-                        }) 
-                    new_sub = subscription_result.is_success                   
-            # else create an subscription
-            else:
+                        })
+                    if subscription_result.is_success:
+                        did_create_sub = True
 
-                subscription_result = braintree.Subscription.create({
-                    "payment_method_token": the_token,
-                    "plan_id":PLAN_ID,
-                    })
-                new_sub = subscription_result.is_success
+                if did_update_sub and not did_create_sub:
+                    messages.success(request, "You've renewed subscription successed, thanks!")
+                    return redirect('history')
+                elif did_create_sub and not did_update_sub:
+                    merchant_obj.subscription_id = subscription_result.subscription.id 
+                    merchant_obj.plan_id = PLAN_ID
+                    merchant_obj.save()
 
-            if new_sub:
-                merchant_obj.subscription_id = subscription_result.subscription.id 
-                merchant_obj.plan_id = PLAN_ID
-                merchant_obj.save()
-
-                payment_type = subscription_result.subscription.transactions[0].payment_instrument_type
-                trans_id = subscription_result.subscription.transactions[0].id
-                sub_amount = subscription_result.subscription.price
-                # create new trans base on payment type
-                if payment_type == braintree.PaymentInstrumentType.PayPalAccount:
-                    newtrans = Transaction.objects.create_newtrans(
-                    user=request.user,
-                    transaction_id=trans_id,
-                    amount=sub_amount,
-                    card_type='paypal',)
-                    trans_success = newtrans.success
-                elif payment_type == braintree.PaymentInstrumentType.CreditCard:
-                    credit_card_details = subscription_result.subscription.transactions[0].credit_card_details
-                    card_type = credit_card_details.card_type
-                    last_4 = credit_card_details.last_4
-                    newtrans = Transaction.objects.create_newtrans(
-                    user=request.user,
-                    transaction_id=trans_id,
-                    amount=sub_amount,
-                    card_type=card_type,
-                    last_four=last_4)
-                    trans_success = newtrans.success
+                    payment_type = subscription_result.subscription.transactions[0].payment_instrument_type
+                    trans_id = subscription_result.subscription.transactions[0].id
+                    sub_amount = subscription_result.subscription.price
+                    # create new trans base on payment type
+                    if payment_type == braintree.PaymentInstrumentType.PayPalAccount:
+                        newtrans = Transaction.objects.create_newtrans(
+                        user=request.user,
+                        transaction_id=trans_id,
+                        amount=sub_amount,
+                        card_type='paypal',)
+                        trans_success = newtrans.success
+                    elif payment_type == braintree.PaymentInstrumentType.CreditCard:
+                        credit_card_details = subscription_result.subscription.transactions[0].credit_card_details
+                        card_type = credit_card_details.card_type
+                        last_4 = credit_card_details.last_4
+                        newtrans = Transaction.objects.create_newtrans(
+                        user=request.user,
+                        transaction_id=trans_id,
+                        amount=sub_amount,
+                        card_type=card_type,
+                        last_four=last_4)
+                        trans_success = newtrans.success
+                    else:
+                        trans_success = False
                 else:
-                    trans_success = False
+                    error_ms = subscription_result.message
+                    messages.error(request, "An error occured: %s" % (error_ms))
+                    return redirect('upgrade')
+            
                 # create membership obj and send update signal
                 if trans_success:
                     membership_instance, created = Membership.objects.get_or_create(
@@ -129,10 +134,7 @@ def upgrade(request):
                 else:
                     messages.error(request, 'There was an error with your trans, please contact us.')
                     return redirect('upgrade')
-            else:
-                error_ms = subscription_result.message
-                messages.error(request, "An error occured: %s" % (error_ms))
-                return redirect('upgrade')
+                
         # send a client token 
         client_token = braintree.ClientToken.generate({
             "customer_id": merchant_customer_id
